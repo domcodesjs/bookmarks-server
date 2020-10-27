5efb34e146d591432d850697e92640ee66341a65
Dominic Faulring <domcodesjs@gmail.com>
2020-10-27 07:57:09 -0700

    edited config

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 module.exports = {
   DB_URL: process.env.DB_URL,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  PORT: process.env.PORT
+  PORT: process.env.PORT || 4000,
+  API_KEY: process.env.API_KEY
 };
